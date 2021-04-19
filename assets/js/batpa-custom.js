c505660f79a042f5d7c15981012abe6b98349f1a
themeplugs <shagor@themeplugs.com>
2021-04-20 01:34:34 +0600

    focus issue fixed

--- assets/js/batpa-custom.js
+++ assets/js/batpa-custom.js
@@ -1,21 +1,21 @@
 jQuery(function($){
- 	"use strict";
-   	jQuery('.gb_navigation > ul').superfish({
-		delay:       500,
-		animation:   {opacity:'show',height:'show'},  
-		speed:       'fast'
-  	});
+	"use strict";
+	  jQuery('.gb_navigation > ul').superfish({
+	   delay:       500,
+	   animation:   {opacity:'show',height:'show'},  
+	   speed:       'fast'
+	 });
 });
 
 function batpa_gb_Menu_open() {
-	jQuery(".side_gb_nav").addClass('show');
+   jQuery(".side_gb_nav").addClass('show');
 }
 function batpa_gb_Menu_close() {
-	jQuery(".side_gb_nav").removeClass('show');
+   jQuery(".side_gb_nav").removeClass('show');
 }
 
 jQuery(function($){
-	$('.gb_toggle').click(function () {
-        batpa_Keyboard_loop($('.side_gb_nav'));
-    });
+   $('.gb_toggle').click(function () {
+	   batpa_Keyboard_loop($('.side_gb_nav'));
+   });
 });
